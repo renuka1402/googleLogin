2a93cc7bad2977db2d10b82571ebd7a8bcff2ed0
renuka sharma <renu66138@gmail.com>
2024-08-02 21:33:15 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import LogoutButton from './logout';
 function App() {
+  const {user,loginWithRedirect } = useAuth0();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={(e) => loginWithRedirect()}>Login with redirect</button>
+      <LogoutButton/>
     </div>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-74ztcphh8gvik60m.us.auth0.com"
+    clientId="rZHbwgYpiZKVDXWT0atEHx8zi0fQEEXP"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>,
+   
   </React.StrictMode>
 );
 
